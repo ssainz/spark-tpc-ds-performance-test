54df16de8a37ef04ce5c55ca3531e3d5d7272b7c
ssainz <sergio.sainz@gmail.com>
2018-06-12 12:17:37 -0400

    adding utils

---
+++ bin/tpcd_util.sh
@@ -0,0 +1,40 @@
+set_environment() {
+  bin_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  script_dir="$(dirname "$bin_dir")"
+  
+  if [ -z "$TPCDS_ROOT_DIR" ]; then
+     TPCDS_ROOT_DIR=${script_dir}
+  fi  
+  if [ -z "$TPCDS_LOG_DIR" ]; then
+     TPCDS_LOG_DIR=${script_dir}/log
+  fi  
+  if [ -z "$TPCDS_DBNAME" ]; then
+     TPCDS_DBNAME="TPCDS"
+  fi  
+  if [ -z "$TPCDS_GENDATA_DIR" ]; then
+     TPCDS_GENDATA_DIR=${TPCDS_ROOT_DIR}/gendata
+  fi  
+  if [ -z "$TPCDS_GEN_QUERIES_DIR" ]; then
+     TPCDS_GENQUERIES_DIR=${TPCDS_ROOT_DIR}/genqueries
+  fi  
+  if [ -z "$TPCDS_WORK_DIR" ]; then
+     TPCDS_WORK_DIR=${TPCDS_ROOT_DIR}/work
+  fi  
+}
+
+set_env() {
+  # read -n1 -s
+  TEST_ROOT=`pwd`
+  set_environment
+  . $TPCDS_ROOT_DIR/bin/tpcdsenv.sh
+  echo "SPARK_HOME is " $SPARK_HOME
+  set_environment
+}
+
+DRIVER_OPTIONS="--driver-java-options -Dlog4j.configuration=file:///${output_dir}/log4j.properties"
+EXECUTOR_OPTIONS="--conf spark.executor.extraJavaOptions=-Dlog4j.configuration=file:///${output_dir}/log4j.properties"
+
+bin/spark-sql ${DRIVER_OPTIONS} ${EXECUTOR_OPTIONS} --conf spark.sql.catalogImplementation=hive -f ${TPCDS_WORK_DIR}/create_database.sql > ${TPCDS_WORK_DIR}/create_database.out 2>&1
+bin/spark-sql ${DRIVER_OPTIONS} ${EXECUTOR_OPTIONS} --conf spark.sql.catalogImplementation=hive -f ${TPCDS_WORK_DIR}/call_center_text_only.sql > ${TPCDS_WORK_DIR}/call_center_text_only.out 2>&1
+
+
